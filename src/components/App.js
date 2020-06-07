66b43ab939dcc7f97c1ae2fb265eb79d37053e0d
Whitney <whitzhu@gmail.com>
2020-06-07 11:23:35 -0700

    [Refactor][Components] Moved App.js and created VideoUploader.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import VideoUploader from "./VideoUploader";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mirror Tiktok Video</h1>
+      <VideoUploader />
+    </div>
+  );
+}
+
+export default App;
